fac0ac559c4bcca2d9682bb02a15b6ea4970e953
preet5477 <cpreet579@gmail.com>
2024-09-12 13:23:11 +0530

    context api project

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Profile from './components/Profile'
+import Login from './components/Login'
+
+function App() {
+
+  
+   return (
+    <UserContextProvider>
+      <h1>Context Api</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
